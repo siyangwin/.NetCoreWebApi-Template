878b0e1fedfd5d555557caf9f1571d68b59e708f
SiYang <59567021+siyangwin@users.noreply.github.com>
2023-05-14 22:57:46 +0800

    [2023-05-14]1.将SerilLog 放置到Service中引用 2.项目内引用Service即可注入SerilLog 3.静态调用

--- Project.AppApi/Program.cs
+++ Project.AppApi/Program.cs
@@ -1,20 +1,13 @@
 using Core;
-using MvcCore.Extension.AutoFac;
 using MvcCore.Extension.Swagger;
 using IService;
 using Service;
-using Autofac.Core;
-using Microsoft.AspNetCore.Mvc.Filters;
 using MvcCore.Extension.Filter;
-using Microsoft.Extensions.Configuration;
-using MvcCore.Extension.SeriLog;
+using Serilog;
 
 var ApiName = "Project.AppApi";
 
 var builder = WebApplication.CreateBuilder(args);
-
-//注入SerilogLog
-builder.AddSeriLog();
 
 //获取连接字符串
 GlobalConfig.ConnectionString = builder.Configuration.GetValue<string>("ConnectionStrings:SqlServer");
@@ -45,6 +38,50 @@ builder.Services.AddMvc(options =>
     //错误拦截器
     options.Filters.Add(typeof(ErrorFilterAttribute));
 });
+
+
+//SerilLog  再Service中引用次NuGet包
+//ThreadId需要引用专用的NuGet包
+//const string OUTPUT_TEMPLATE = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} <{ThreadId}> [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+const string OUTPUT_TEMPLATE = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+
+Log.Logger = new LoggerConfiguration()
+    //.MinimumLevel.Debug() //设置日志记录器的最小级别为 Debug，即只记录 Debug、Information、Warning、Error 和 Fatal 级别的日志事件。
+    //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)//对 Microsoft 命名空间下的所有日志事件进行重写，将最小级别设置为 Information，即只记录 Information、Warning、Error 和 Fatal 级别的日志事件。
+    //.Enrich.FromLogContext() //启用日志上下文功能，自动获取当前线程和方法的一些信息，并添加到每个日志事件中。
+    //.WriteTo.Console(outputTemplate: OUTPUT_TEMPLATE)
+    .WriteTo.File("logs/{Date}/app.txt"
+        , rollingInterval: RollingInterval.Day,
+         rollOnFileSizeLimit: true, // 当日志文件大小超过指定大小时自动滚动日志文件
+         fileSizeLimitBytes: 1048576, // 日志文件最大大小为 1MB
+          retainedFileCountLimit: 7, // 最多保留 7 天的日志文件
+          outputTemplate: OUTPUT_TEMPLATE)
+    .CreateLogger();
+
+///写入数据库需要引用专用的NuGet包
+//string connectionString = "Data Source=localhost;Initial Catalog=Logs;Integrated Security=True";
+
+//Log.Logger = new LoggerConfiguration()
+//    .WriteTo.MSSqlServer(
+//        connectionString,
+//        sinkOptions: new MSSqlServerSinkOptions { TableName = "LogEvents" },
+//        columnOptions: new ColumnOptions
+//        {
+//            AdditionalDataColumns = new Collection<DataColumn>
+//            {
+//    new DataColumn { DataType = typeof(string), ColumnName = "Application" }
+//            }
+//        })
+//    .CreateLogger();
+
+//写入Log案例
+//Log.Information("Hello, Serilog!");
+//Log.Error("err");
+///Log.CloseAndFlush();
+
+//注入
+builder.Host.UseSerilog(Log.Logger, dispose: true);
 
 builder.Services.AddControllersWithViews();
 
